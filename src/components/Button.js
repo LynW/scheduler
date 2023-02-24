2f891dfe0d6723a67de78a102d0557b907cfb8d1
lynw <c54wang@gmail.com>
2023-02-24 13:46:49 -0500

    adjust to camelCase

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,11 @@
 import React from "react";
-import classnames from "classnames";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
 
-  let buttonClass = classnames("button", {" button--confirm": props.confirm}, {' button--danger': props.danger} );
+  let buttonClass = classNames("button", {" button--confirm": props.confirm}, {' button--danger': props.danger} );
 
   // if(props.confirm) {
   //   buttonClass += " button--confirm";
