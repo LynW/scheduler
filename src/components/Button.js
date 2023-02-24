3c24c2c905c25237595c68e45df149d30e2e9f4a
lynw <c54wang@gmail.com>
2023-02-24 13:45:10 -0500

    button test + refactor button with classnames

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,17 @@
 import React from "react";
+import classnames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-  let buttonClass = "button";
 
-  if(props.confirm) {
-    buttonClass += " button--confirm";
-  } else if (props.danger) {
-    buttonClass += " button--danger";
-  }
+  let buttonClass = classnames("button", {" button--confirm": props.confirm}, {' button--danger': props.danger} );
+
+  // if(props.confirm) {
+  //   buttonClass += " button--confirm";
+  // } else if (props.danger) {
+  //   buttonClass += " button--danger";
+  // }
  
    return (
     <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>
