b728a04fa671e9179bda38ec88d64a9e36a58942
lynw <c54wang@gmail.com>
2023-03-14 21:51:40 -0400

    Add fragments and show student on Appointment story

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -167,9 +167,34 @@ storiesOf("Appointment", module)
     />
   ))
   .add("Edit", () => (
-    <Form name="Lydia Miller-Jones" interviewers={interviewers} interviewer={4} onSave={action("onSave")} onCancel={action("onCancel")} 
+    <Form
+      name="Lydia Miller-Jones"
+      interviewers={interviewers}
+      interviewer={4}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ))
   .add("Create", () => (
-    <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
   ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewers }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ));
