48cbb9b07c1041379e4232694f31b66903c7a08b
lynw <c54wang@gmail.com>
2023-03-14 23:05:56 -0400

    import Appointment to Application and iterate them

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import "components/Application.scss";
+import Appointment from "components/Appointment";
 import DayList from "./DayList";
 
 const days = [
@@ -15,16 +16,57 @@ const days = [
     spots: 5,
   },
   {
-      id: 3,
-      name: "Wednesday",
+    id: 3,
+    name: "Wednesday",
     spots: 0,
   },
 ];
 
+const appointments = {
+  1: {
+    id: 1,
+    time: "12pm",
+  },
+  2: {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  3: {
+    id: 3,
+    time: "2pm",
+  },
+  4: {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  5: {
+    id: 5,
+    time: "4pm",
+  },
+};
+
 export default function Application(props) {
-  
   const [day, setDay] = useState("Monday");
-  const [interviewer, setInterviewer] = useState(null);
+
+  const appointmentList = Object.values(appointments).map((myKey) => {
+    return <Appointment key={myKey.id} {...myKey} />;
+  });
 
   return (
     <main className="layout">
@@ -36,11 +78,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            value={day}
-            onChange={setDay}
-          />
+          <DayList days={days} value={day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -49,7 +87,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
