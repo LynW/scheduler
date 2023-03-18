89993536ede5ee0dc66758f6aeae2141aacefac5
lynw <c54wang@gmail.com>
2023-03-18 17:52:24 -0400

    fix story for InterviewerListItem

--- stories/index.js
+++ stories/index.js
@@ -78,6 +78,12 @@ storiesOf("DayList", module)
     <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
   ));
 
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png",
+};
+
 storiesOf("InterviewerListItem", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
