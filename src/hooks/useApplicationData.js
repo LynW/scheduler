8d5b2992465059ffd221925eec2bd5f1f260ba0c
lynw <c54wang@gmail.com>
2023-03-19 00:14:16 -0400

    clean up

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -37,13 +37,6 @@ export default function useApplicationData() {
       [id]: appointment,
     };
 
-    const days = state.days.map((day) => {
-      if (day.name === state.day && !state.appointments[id].interview) {
-        day.spots--;
-      }
-      return day;
-    });
-
     return axios
       .put(`http://localhost:8001/api/appointments/${id}`, { interview })
       .then((res) => {
@@ -74,12 +67,6 @@ export default function useApplicationData() {
             appointments,
           });
         }
-        const days = state.days.map((day) => {
-          if (day.name === state.day) {
-            day.spots++;
-          }
-          return day;
-        });
       });
   }
   return { state, setState, bookInterview, cancelInterview, setDay };
