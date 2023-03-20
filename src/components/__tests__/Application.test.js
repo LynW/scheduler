42aedab67b5555308ec90c4f10beda233d4c75ed
lynw <c54wang@gmail.com>
2023-03-20 10:58:56 -0400

    add tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,14 @@
 import React from "react";
-
 import { render, cleanup } from "@testing-library/react";
-
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-xit("renders without crashing", () => {
-  render(<Application />);
+describe("Application", () => {
+  xit("renders without crashing", () => {
+    render(<Application />);
+  });
+  it("does something it is supposed to do", () => {
+    // ...
+  });
 });
