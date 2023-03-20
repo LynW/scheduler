1a555f3ab5e37061e7e8f4159e326d1db121d767
lynw <c54wang@gmail.com>
2023-03-20 14:14:45 -0400

    adjust to relative url

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -38,7 +38,7 @@ export default function useApplicationData() {
     };
 
     return axios
-      .put(`http://localhost:8001/api/appointments/${id}`, { interview })
+      .put(`/api/appointments/${id}`, { interview })
       .then((res) => {
         if (res.status === 204) {
           setState({
@@ -51,7 +51,7 @@ export default function useApplicationData() {
 
   function cancelInterview(id) {
     return axios
-      .delete(`http://localhost:8001/api/appointments/${id}`)
+      .delete(`/api/appointments/${id}`)
       .then((res) => {
         if (res.status === 204) {
           const appointment = {
