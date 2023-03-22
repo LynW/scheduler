d9cff549d67279d5bf4fda458cd0113f01b0ba4d
lynw <c54wang@gmail.com>
2023-03-22 13:11:10 -0400

    remove comments

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,14 +5,10 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
   const transition = (mode, replace = false) => {
-    console.log("Transitioned! history ", history)
     if (replace) {
-      console.log("In IF transition: ", history)
       setHistory((prev) => [...prev.slice(0, prev.length - 1), mode])
     } else {
-      console.log("In IF transition is false ", history)
       setHistory((prev) => {
-        console.log("prev and mode", [...prev, mode]);
         return [...prev, mode]
       })
     }
@@ -21,10 +17,7 @@ export default function useVisualMode(initial) {
 
   const back = () => {
     if (history.length > 1) {
-      console.log("In IF back:", history)
       setHistory((prev) => {
-        console.log("Prev: ", prev);
-        console.log("History again: ", history);
         return [...prev.slice(0, -1)]});
       setMode(history[history.length - 2]);
     }
